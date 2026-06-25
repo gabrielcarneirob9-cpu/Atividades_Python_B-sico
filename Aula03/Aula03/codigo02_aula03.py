52c7c8f66d4f946a153c1e33f4ccb7db6d733385
gabrielcarneirob9-cpu <gabrielcarneirob9@gmail.com>
2026-06-25 10:12:13 -0300

    Add codigo02_aula03.py for name formatting challenge

---
+++ Aula03/Aula03/codigo02_aula03.py
@@ -0,0 +1,13 @@
+#PROFESSOR: Rodrigo Medeiros Vilela
+#ALUNA: Gabriel Carneiro Barros 
+#COMPONENTE: Python Básico
+#DATA: 11/06/26
+#AULA 03
+
+# --- DESAFIO PRÁTICO ---
+
+nome_completo=input("Digite o seu nome completo: ")
+nome_limpo=nome_completo.replace("-", " ")
+nome_padronizado=nome_limpo.upper()
+print("Seu nome é:", nome_padronizado)
+print("E tem", len(nome_padronizado), "caracteres")
